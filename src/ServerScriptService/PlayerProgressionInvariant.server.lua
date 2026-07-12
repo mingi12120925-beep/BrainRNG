0a83ebb56d70f1b20f1ed2dfad896c1c6adb5af6
mingi12120925-beep <mingi12120925@gmail.com>
2026-07-12 09:54:43 +0900

    Increase fixed player movement speed to 28

--- src/ServerScriptService/PlayerProgressionInvariant.server.lua
+++ src/ServerScriptService/PlayerProgressionInvariant.server.lua
@@ -6,7 +6,7 @@
 
 local Players = game:GetService("Players")
 
-local FIXED_WALK_SPEED = 22
+local FIXED_WALK_SPEED = 28
 local FIXED_JUMP_POWER = 50
 local FIXED_JUMP_HEIGHT = 7.2
 local APPEARANCE_WAIT_SECONDS = 8
